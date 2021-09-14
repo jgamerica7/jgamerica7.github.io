1015d74c9b680ca0d54b31a6608ce4774f69045f
jgamerica7 <jyotigiri54@gmail.com>
2021-09-13 20:07:21 -0400

    Loop over an Array

--- cssind/Module_4_Assignment1/script.js
+++ cssind/Module_4_Assignment1/script.js
@@ -1,14 +1,14 @@
 var names=new Array();
-names[0]="Yaakov";
-names[1]="John";
-names[2]="Jen";
-names[3]="Jason";
-names[4]="Paul";
-names[5]="Frank";
-names[6]="Larry";
-names[7]="Paula";
-names[8]="Laura";
-names[9]="Jim";
+names[0]="Andrea";
+names[1]="Jeremie";
+names[2]="Bryn";
+names[3]="John";
+names[4]="Marry";
+names[5]="Kristina";
+names[6]="Joe";
+names[7]="William";
+names[8]="Julia";
+names[9]="Jasmine";
 
 
 for (var i = 0; i < names.length; i++) {
